4f1a31d027b865f5c4d8f26dcd9673c3df2282f9
redwanrahman <rrtonmoy@gmail.com>
2021-04-19 16:24:38 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+const temp = document.querySelector('.temp');
+const desc = document.querySelector('.desc');
+const clouds = document.querySelector('.clouds');
+const button= document.querySelector('.submit');
+
+
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=youropenweatherApi key input here')
+.then(response => response.json())
+.then(data => {
+  const tempValue = data['main']['temp'];
+  const nameValue = data['name'];
+  const descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  input.value ="";
+
+})
+
+.catch(err => alert("Wrong city name!"));
+})
